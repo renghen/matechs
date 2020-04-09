1cb5799857abe548fc985f1c30ecb460cecd87ba
renghen <renghen@yahoo.com>
2020-04-09 21:47:43 +0400

    jest added

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rootDir: './',
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['packages/**/src/**/*.ts'],
+  setupFiles: ['./jest-setup.ts'],
+  verbose: true
+}
